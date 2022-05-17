14af802dc895d9509212833d5c1c9c9eb7690e34
kriuchkovaleks <103981431+kriuchkovaleks@users.noreply.github.com>
2022-05-17 20:26:13 +0300

    Add files via upload

---
+++ Task10.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Сгенерированное число: {number}");
+
+
+int secondAndThirdDigit = number % 100;
+
+
+int firstdigitOFsecondAndThirdDigit = secondAndThirdDigit / 10;
+
+
+Console.WriteLine($"Итоговое число: {firstdigitOFsecondAndThirdDigit}");
+
+
+
